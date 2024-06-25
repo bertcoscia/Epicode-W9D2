d12138613c2cb0a041e2fde046693a375493079b
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-25 15:26:53 +0200

    adds footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
     <div className="App">
       <MyNav />
+      <MyFooter />
     </div>
   );
 }
